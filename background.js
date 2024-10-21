440a337abd2780a54e0c1e7a2941387867db0659
Guilherme Almeida <xdgrxd.dev@gmail.com>
2024-10-20 21:43:34 -0300

    link survey update

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ chrome.runtime.onInstalled.addListener(function (details) {
   /* other 'reason's include 'update' */
   if (details.reason == "install") {
     /* If first install, set uninstall URL */
-    const uninstallUrlLink = "https://forms.gle/bwxVGugEjSTqLDBy9";
+    const uninstallUrlLink = "https://forms.gle/v6263P2BFxhAhy8bA";
     /* If Chrome version supports it... */
     if (chrome.runtime.setUninstallURL) {
       chrome.runtime.setUninstallURL(uninstallUrlLink);
