76d5bc94bbc8e58ffe59b21ba32457f6e84eef1e
Guilherme Almeida <xdgrxd.dev@gmail.com>
2024-10-31 22:20:57 -0300

    some fix and form update

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ chrome.runtime.onInstalled.addListener(function (details) {
   /* other 'reason's include 'update' */
   if (details.reason == "install") {
     /* If first install, set uninstall URL */
-    const uninstallUrlLink = "https://forms.gle/v6263P2BFxhAhy8bA";
+    const uninstallUrlLink = "https://forms.gle/mmbmMYYx9LVyMsqUA";
     /* If Chrome version supports it... */
     if (chrome.runtime.setUninstallURL) {
       chrome.runtime.setUninstallURL(uninstallUrlLink);
